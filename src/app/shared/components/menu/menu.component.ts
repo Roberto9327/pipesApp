40c00529e684c807ef7d2b32cbbdaff3e388f296
Roberto Rivero <roberivero@nuevatel.net>
2023-07-31 16:18:12 -0400

    menu-bar

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -14,38 +14,32 @@ export class MenuComponent  implements OnInit{
   ngOnInit() {
       this.Items = [
           {
-              label: 'Options',
+              label: 'Pipes de Angular',
+              icon: 'pi pi-desktop',
               items: [
                   {
-                      label: 'Update',
-                      icon: 'pi pi-refresh',
-                      command: () => {
-                          this.update();
-                      }
+                      label: 'Textos y Flechas',
+                      icon: 'pi pi-align-left',
+                  },
+                  {
+                      label: 'Números',
+                      icon: 'pi pi-dollar',
                   },
                   {
                       label: 'Delete',
-                      icon: 'pi pi-times',
-                      command: () => {
-                          this.delete();
-                      }
+                      icon: 'pi pi-globe',
                   }
               ]
           },
           {
-              label: 'Navigate',
-              items: [
-                  {
-                      label: 'Angular',
-                      icon: 'pi pi-external-link',
-                      url: 'http://angular.io'
-                  },
-                  {
-                      label: 'Router',
-                      icon: 'pi pi-upload',
-                      routerLink: '/fileupload'
-                  }
-              ]
+            label: 'Pipes Personalizados',
+            icon: 'pi pi-cog',
+            items: [
+              {
+              label: 'Otro elemento',
+              icon:  'pi pi-cog',
+              }
+            ]
           }
       ];
   }
